53acdd923cdda55201e9c5d6396b44ad8050b61b
Tamplier <araman777@gmail.com>
2026-03-05 13:02:27 +0500

    Fixes

--- app/jobs/steam/update_list_job.rb
+++ app/jobs/steam/update_list_job.rb
@@ -3,7 +3,7 @@
 module Steam
   class UpdateListJob < ApplicationJob
     queue_as :low_priority
-    limits_concurrency to: 2, duration: 1.hour
+    limits_concurrency key: ->(job) { job.class.name }, to: 2, duration: 1.hour
     retry_on Faraday::Error, wait: 6.minutes, attempts: 5
 
     def perform
--- app/services/steam/update_list_service.rb
+++ app/services/steam/update_list_service.rb
@@ -7,12 +7,12 @@ module Steam
     def initialize(limit: LIST_LIMIT)
       super()
       @limit = limit
+      @store = Store.steam
     end
 
     def call
       client = ::Steam::Client.new
       last_appid = ExternalId.steam.order(created_at: :desc).pick(:external_id)
-      store = Store.steam
       processed_games = 0
 
       loop do
@@ -22,14 +22,7 @@ module Steam
         more_results = response.dig('response', 'have_more_results')
         break if app_list.blank?
 
-        app_list.each_slice(1_000) do |batch|
-          games = batch.map do |p|
-            game = Game.new(title: p['name'], last_modified: p['last_modified'])
-            game.external_ids.build(store: store, external_id: p['appid'])
-            game
-          end
-          Game.import(games, recursive: true, validate: false)
-        end
+        process_list(app_list)
         processed_games += app_list.length
         Rails.logger.info("Steam::UpdateListService processed #{processed_games} records")
 
@@ -37,6 +30,19 @@ module Steam
       end
 
       success
+    end
+
+    private
+
+    def process_list(app_list)
+      app_list.each_slice(1_000) do |batch|
+        games = batch.map do |p|
+          game = Game.new(title: p['name'], last_modified: p['last_modified'])
+          game.external_ids.build(store: @store, external_id: p['appid'])
+          game
+        end
+        Game.import(games, recursive: true, validate: false)
+      end
     end
   end
 end
